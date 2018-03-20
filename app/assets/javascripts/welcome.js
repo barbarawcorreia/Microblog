d3ec4726f8f14de2ad212c23000cf8179cb143d2
Bárbara Correia <barbara.correia@corp.globo.com>
2018-03-20 13:50:46 -0300

    Comments were deleted

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,19 +1,33 @@
-function load_obj_json() {
+function load_posts() {
     var container;
+    var cards_element = document.getElementById("cards"); // Pega cards como Pai do elemento que será criado dentro dele card
+
     $.getJSON("/myjson.json", function (posts) {
         for (let post of posts) {
 
+            var card = document.createElement("div");
+            cards_element.appendChild(card);
+            card.className = "card pink lighten-2 center-align condensed light card-content white-text";
+
+            var card_title = document.createElement("span");
+            card.appendChild(card_title);
+            card_title.className = "card-title";
+            card_title.textContent = "POST";
+
             var div = document.createElement("div");
-            container = document.getElementById("posts");
-            container.appendChild(div);
-            var p = document.createElement("p");
+            card.appendChild(div);
+
+            let p = document.createElement("p");
             div.appendChild(p);
-            p.textContent = 'Title : ' + post.title;
-            var p2 = document.createElement("p");
-            div.appendChild(p2);
-            p2.textContent = 'Text' + post.text;
+            p.textContent = 'TITLE: ' + post.title;
+
+            p = document.createElement("p");
+            div.appendChild(p);
+            p.textContent = 'TEXT: ' + post.text;
 
         }
+    window.scroll(0,130000);
     });
+
 }
-load_obj_json();
+document.getElementsByClassName("open-blog")[0].onclick = load_posts;
